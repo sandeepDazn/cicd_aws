f44787ccbc31111c3f5fe86f4e21f989b685e6c6
sandeepDazn <141609285+sandeepDazn@users.noreply.github.com>
2024-08-28 14:40:26 +0530

    path modified ......

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_iam_role" "lambda_execution_role" {
-  name = "lambda_execution_role_v2"
+  name = "lambda_execution_role_v100"
 
   assume_role_policy = jsonencode({
     "Version": "2012-10-17",
