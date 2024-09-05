4d99b5a08e9b62e048be5318b13c2d3104576501
sandeepDazn <141609285+sandeepDazn@users.noreply.github.com>
2024-09-05 15:21:16 +0530

    newly cloned main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,65 +11,46 @@ resource "aws_s3_bucket" "new-bucket" {
 
 # newly added file
 
-resource "aws_lambda_function" "my_lambda" {
-  function_name     = "my-lambda-function-sep"
-  description       = "A Terraform-managed Lambda function"
-  runtime           = "python3.9"
-  role              = aws_iam_role.lambda_role.arn
-  handler           = "lambda_function.handler"
-  code {
-    zip_file = file("../lambda_one.zip")
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
   }
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+data "archive_file" "lambda" {
+  type        = "zip"
+  source_file = "../lambda/index.js"
+  output_path = "../ambda_function_payload.zip"
+}
+
+resource "aws_lambda_function" "test_lambda" {
+  # If the file is not in the current working directory you will need to include a
+  # path.module in the filename.
+  filename      = "lambda_function_payload.zip"
+  function_name = "lambda_function_name"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "index.test"
+
+  source_code_hash = data.archive_file.lambda.output_base64sha256
+
+  runtime = "nodejs18.x"
 
   environment {
     variables = {
-      MY_ENV_VAR = "value"
+      foo = "bar"
     }
   }
 }
-
-resource "aws_iam_role" "lambda_role" {
-  name = "lambda-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect    = "Allow",
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        },
-        Action    = "sts:AssumeRole"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy" "lambda_role_policy" {
-  name = "lambda-role-policy"
-  role = aws_iam_role.lambda_role.name
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect    = "Allow",
-        Action    = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ],
-        Resource  = "arn:aws:logs:*:*:log-group:/aws/lambda/my-lambda-function*"
-      }
-    ]
-  })
-}
-# resource "aws_lambda_function" "lambda_name" {
-#   function_name = "007-lambda"
-# #   role = ""
-# handler = "index.handler"
-# role = ""
-# }
-# data "" "name" {
-
-# }
