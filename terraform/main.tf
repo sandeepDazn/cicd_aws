893982b3af2a50502385b010f3784b28695dba38
sandeepDazn <141609285+sandeepDazn@users.noreply.github.com>
2024-09-05 15:25:05 +0530

    role name modifieded

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,7 +39,7 @@ data "archive_file" "lambda" {
 resource "aws_lambda_function" "test_lambda" {
   # If the file is not in the current working directory you will need to include a
   # path.module in the filename.
-  filename      = "lambda_function_payload.zip"
+  filename      = "../lambda_function_payload.zip"
   function_name = "lambda_function_name"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "index.test"
