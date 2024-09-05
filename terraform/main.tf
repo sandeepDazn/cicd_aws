83b01630a287554dc63fbcb4b8f4dbcfe79a038b
sandeepDazn <141609285+sandeepDazn@users.noreply.github.com>
2024-09-05 15:23:48 +0530

    role name modified

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,7 +26,7 @@ data "aws_iam_policy_document" "assume_role" {
 }
 
 resource "aws_iam_role" "iam_for_lambda" {
-  name               = "iam_for_lambda"
+  name               = "iam_for_lambda_testing-007"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
