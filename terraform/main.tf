02d50e3246b1232851fc3d22415d708fc27dfc92
sandeepDazn <141609285+sandeepDazn@users.noreply.github.com>
2024-09-05 23:38:24 +0530

    lifecycle syntax is added-1

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,7 +16,7 @@ data "aws_iam_policy_document" "assume_role" {
 }
 
 resource "aws_iam_role" "iam_for_lambda" {
-  name               = "iam_for_lambda_testing-00-007"
+  name               = "iam_for_lambda_testing-00-008"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
   lifecycle {
     create_before_destroy = true
