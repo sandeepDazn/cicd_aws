f355478468c029227ea68434234f040206aa0d04
sandeepDazn <141609285+sandeepDazn@users.noreply.github.com>
2024-09-05 15:10:40 +0530

    zip path modified

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,6 +8,62 @@ resource "aws_s3_bucket" "new-bucket" {
         second = "second tag"
     }
 }
+
+# newly added file
+
+resource "aws_lambda_function" "my_lambda" {
+  function_name     = "my-lambda-function-sep"
+  description       = "A Terraform-managed Lambda function"
+  runtime           = "python3.9"
+  role              = aws_iam_role.lambda_role.arn
+  handler           = "lambda_function.handler"
+  code {
+    zip_file = file("../lambda_one.zip")
+  }
+
+  environment {
+    variables = {
+      MY_ENV_VAR = "value"
+    }
+  }
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect    = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        },
+        Action    = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "lambda_role_policy" {
+  name = "lambda-role-policy"
+  role = aws_iam_role.lambda_role.name
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect    = "Allow",
+        Action    = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Resource  = "arn:aws:logs:*:*:log-group:/aws/lambda/my-lambda-function*"
+      }
+    ]
+  })
+}
 # resource "aws_lambda_function" "lambda_name" {
 #   function_name = "007-lambda"
 # #   role = ""
