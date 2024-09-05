f36cd5a7ae97e1bb4bd3f4d6472a9a395e7eecbc
sandeepDazn <141609285+sandeepDazn@users.noreply.github.com>
2024-09-05 15:33:36 +0530

    zip path modified

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,20 +26,20 @@ data "aws_iam_policy_document" "assume_role" {
 }
 
 resource "aws_iam_role" "iam_for_lambda" {
-  name               = "iam_for_lambda_testing-00-002"
+  name               = "iam_for_lambda_testing-00-003"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
 data "archive_file" "lambda" {
   type        = "zip"
-  source_file = "../lambda/index.js"
-  output_path = "../ambda_function_payload.zip"
+  source_file = "lambda/index.js"
+  output_path = "ambda_function_payload.zip"
 }
 
 resource "aws_lambda_function" "test_lambda" {
   # If the file is not in the current working directory you will need to include a
   # path.module in the filename.
-  filename      = "../lambda_function_payload.zip"
+  filename      = "lambda_function_payload.zip"
   function_name = "lambda_function_name"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "index.test"
