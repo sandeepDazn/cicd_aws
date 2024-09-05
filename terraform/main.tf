63473a15c2ff6429e2c66e6fdbc69ea460d4a3bb
sandeepDazn <141609285+sandeepDazn@users.noreply.github.com>
2024-09-05 15:22:25 +0530

    comment s3 buccket code

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
   region = "ap-northeast-1"
 }
-resource "aws_s3_bucket" "new-bucket" {
-    bucket = "new-bucket-007-007-111"
-    tags = {
-        first = "first tag"
-        second = "second tag"
-    }
-}
+# resource "aws_s3_bucket" "new-bucket" {
+#     bucket = "new-bucket-007-007-111"
+#     tags = {
+#         first = "first tag"
+#         second = "second tag"
+#     }
+# }
 
 # newly added file
 
