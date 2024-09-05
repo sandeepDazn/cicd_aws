0ed321c00aab847fb7a9aeafd545f7a12710d34c
sandeepDazn <141609285+sandeepDazn@users.noreply.github.com>
2024-09-05 22:19:37 +0530

    room commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,7 +26,7 @@ data "aws_iam_policy_document" "assume_role" {
 }
 
 resource "aws_iam_role" "iam_for_lambda" {
-  name               = "iam_for_lambda_testing-00-005"
+  name               = "iam_for_lambda_testing-00-006"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
@@ -40,9 +40,9 @@ resource "aws_lambda_function" "test_lambda" {
   # If the file is not in the current working directory you will need to include a
   # path.module in the filename.
   filename      = "lambda_function_payload.zip"
-  function_name = "lambda_function_name"
+  function_name = "lambda_room_function_name"
   role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "index.test"
+  handler       = "index.handler"
 
   source_code_hash = data.archive_file.lambda.output_base64sha256
 
