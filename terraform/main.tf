93e0f277230b7ab85eee4f73318e7c15e6d88083
sandeepDazn <141609285+sandeepDazn@users.noreply.github.com>
2024-09-05 15:01:22 +0530

    111 file name added

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,9 +2,18 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 resource "aws_s3_bucket" "new-bucket" {
-    bucket = "new-bucket-007-007"
+    bucket = "new-bucket-007-007-111"
     tags = {
         first = "first tag"
         second = "second tag"
     }
 }
+# resource "aws_lambda_function" "lambda_name" {
+#   function_name = "007-lambda"
+# #   role = ""
+# handler = "index.handler"
+# role = ""
+# }
+# data "" "name" {
+
+# }
